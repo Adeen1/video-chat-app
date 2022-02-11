aaa6e1f5eae5d2a2decb7ccff5508d5ce3d9cd07
Ahmad <adeenahmad56@gmail.com>
2022-02-11 16:17:40 +0500

    aghh

--- index.js
+++ index.js
@@ -11,24 +11,26 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.send("Running");
 });
 
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
+
   socket.on("disconnect", () => {
-    socket.broadcast.emit("Call Ended");
+    socket.broadcast.emit("callEnded");
   });
+
   socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("callUser", { signal, signalData, from, name });
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
+
   socket.on("answerCall", (data) => {
     io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log("sever is listening on port " + PORT);
-});
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
